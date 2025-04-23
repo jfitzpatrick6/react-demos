822a82059de31aec229070b1fcace9db31779932
jfitzpatrick6 <112958802+jfitzpatrick6@users.noreply.github.com>
2025-04-23 13:35:48 -0400

    Next Challenge

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,15 @@ import Greeting from './components/Greeting.jsx';
 import ProductInfo from './components/ProductInfo.jsx';
 import UserList from './components/UserList.jsx';
 import ProductList from './components/ProductList.jsx';
+import Person from './components/Person.jsx';
+import Product from './components/Product.jsx';
 
 const App = () => {
   
   return (
     <div>
-      <UserList/>
-      <ProductList/>
+      <Person name="jake" age={25}/>
+      <Product name="Laptop" price={2450}/>
     </div>
   )
 }
---
+++ src/components/Product.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Product({name, price}) {
+  return (
+    <div>
+        <h2>{name}</h2>
+        <p>{price}</p>
+    </div>
+  )
+}
+
+export default Product
